7621d5211bf2070b2277917900da33f8f3ab276f
yahindanil <ahin.danil@gmail.com>
2024-03-25 17:45:10 +0400

    ts fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,7 +5,9 @@ import { cookies } from "next/headers";
 export function createClient() {
   const cookieStore = cookies();
 
-  return createServerClient<Database>(
+  // Возникает ошибка с тайпскриптом если добавить <Database>
+  // return createServerClient<Database>(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {

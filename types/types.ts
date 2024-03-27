e79456ebc0bac29496fe3b53a82086ee4d762f22
yahindanil <ahin.danil@gmail.com>
2024-03-27 19:04:42 +0400

    types.ts updated

--- types/types.ts
+++ types/types.ts
@@ -4,4 +4,3 @@ import type { User as SupabaseUser } from "@supabase/gotrue-js/src/lib/types";
 export type User = SupabaseUser | null;
 export type Item = Database["public"]["Tables"]["items"]["Row"];
 export type Collection = Database["public"]["Tables"]["collections"]["Row"];
-export type ItemImage = Database["public"]["Tables"]["items_images"]["Row"];
